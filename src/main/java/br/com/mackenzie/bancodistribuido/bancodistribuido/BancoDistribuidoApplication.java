a37e019fffae6e17a5d04351446271f695a0ad2a
vinicius.moraes <vinicius.moraes@moipadmins-MacBook-Pro.local>
2017-08-25 16:04:25 -0300

    inicializando projeto

---
+++ src/main/java/br/com/mackenzie/bancodistribuido/bancodistribuido/BancoDistribuidoApplication.java
@@ -0,0 +1,12 @@
+package br.com.mackenzie.bancodistribuido.bancodistribuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDistribuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDistribuidoApplication.class, args);
+	}
+}
